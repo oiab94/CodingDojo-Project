e7bda9ef0732233e73ea51301408ce8b29868dc3
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-14 15:55:31 -0300

    [components] Maneajador de errores para el signup

---
+++ server/components/handleErrors.js
@@ -0,0 +1,22 @@
+module.exports.handleErrors = function ({errors, message, code}) {
+	const error = {email:"", password: ""};
+
+	// * Validacion de errores al crear usuarios
+	// Dupliacion de email
+	if(code === 11000){
+		error.email = "Thath email is registered";
+		return error;
+	}
+
+	// Verificamos si recibimos un mensaje de validación de usuario
+	if(message.includes("user validation failed"))
+		// Cada objeto con errror tiene un valor properties
+		Object.values(errors).forEach(({ properties }) => {
+			// Estas properties indican donde va ocurriendo los errores
+			error[properties.path] = properties.message; // Guardamos los mensajes de error
+		})
+
+		// * Validacion de errores al hacer login
+
+		return error;
+}
