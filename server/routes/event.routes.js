1d8a3eb0173d078f2d7d16c90e3a1f49ffc6c6ff
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 19:30:55 -0300

    [server] Rutas de acceso a la API

--- server/routes/event.routes.js
+++ server/routes/event.routes.js
@@ -4,5 +4,6 @@ const eventController = require("../controllers/event.controller");
 const router = Router();
 
 router.post("/api/event", eventController.event_post);
+router.get("/api/event", eventController.event_get);
 
 module.exports = router;
