33c96bb2e60f3da0967b222b97781b75f50cef57
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 15:38:34 -0300

    [server] Login para un usuario

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,18 @@
 const { handleErrors } = require("../components/handleErrors");
 const User = require("../models/user.model");
 
+module.exports.login_post = (req, res) => {
+	const { email, password } = req.body;
+
+	User.login(email, password)
+		.then(user => res.status(200).json({ "userID": user._id }))
+		.catch(error => {
+			const errors = handleErrors(error);
+
+			res.status(400).json(errors)
+		});
+}
+
 module.exports.signup_post = (req, res) => {
 	const {
 		firstName,
