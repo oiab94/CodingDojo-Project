e3c0fc9a53a4df904209ef4ff0f5fd5d76e7bd1c
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 15:19:17 -0300

    [client] Utiliza componente basic form

--- client/src/app/view/user/Login.jsx
+++ client/src/app/view/user/Login.jsx
@@ -1,23 +1,20 @@
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Col } from "react-bootstrap";
+import BasicForm from "../../components/user/BasicForm";
 import { Link } from "react-router-dom"
 
 const LoginUser = () => {
 	return (
 		<>
 		<Form className="p-3">
-			<Form.Group as={Row} className="mb-3">
-				<Form.Label column sm={3}>Email address</Form.Label>
-				<Col sm={6}>
-					<Form.Control type="text" placeholder="Enter email" />
-				</Col>
-			</Form.Group>
+			<BasicForm
+				label="Email addres"
+				type="text"
+				placeholder="Enter email address" />
 
-			<Form.Group as={Row} className="mb-3">
-				<Form.Label column sm={3}>Password</Form.Label>
-				<Col sm={6}>
-					<Form.Control type="password" placeholder="Password" />
-				</Col>
-			</Form.Group>
+			<BasicForm
+				label="Password"
+				type="password"
+				placeholder="Password" />
 
 			<Col>
 			<Button as={Link} to="/dashboard" variant="primary" type="submit">
