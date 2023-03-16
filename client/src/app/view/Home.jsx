eae150d336fc1abdfc97d5cf8f04e3260db53e11
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 16:33:12 -0300

    [client] Eliminacion de alerta

--- client/src/app/view/Home.jsx
+++ client/src/app/view/Home.jsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
-import { Container, Row, Col, Alert } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 import CreateUser from "./user/Create"
 import LoginUser from "./user/Login";
 
 const Home = () => {
-	const [show, setShow] = useState(true);
-
 	return (
 		<Container>
 			<Row className="text-center">
-				{
-					show ? 
-						<Alert variant="danger" onClose={() => setShow(false)} dismissible>
-						<Alert.Heading>Noticia</Alert.Heading>
-							<p>La BD todavía no está implementada, solo funciona el FrontEnd a modo de demostración</p>
-						</Alert>:
-						null
-			}
 				<h1>ISport</h1>
 				<h3>Free pickup Game Finder and Organizer</h3>
 			</Row>
