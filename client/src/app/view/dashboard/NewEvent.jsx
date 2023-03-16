91fded658cf3efb49e5047943e557b73af1889e2
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 17:21:18 -0300

    [client] Pagina permite crear nuevo evento

--- client/src/app/view/dashboard/NewEvent.jsx
+++ client/src/app/view/dashboard/NewEvent.jsx
@@ -1,7 +1,42 @@
+import { Form } from "react-bootstrap";
+import BasicForm from "../../components/user/BasicForm";
+
 const NewEvent = () => {
 	return (
 		<>
 			<h1>New Event</h1>
+			<Form>
+				<BasicForm 
+					label="Title"
+					type="text"
+					placeholder="Title" />
+
+				<BasicForm 
+					label="Date"
+					type="date"
+					placeholder="Date" />
+				
+				<BasicForm 
+					label="Time"
+					type="time"
+					placeholder="Time" />
+
+				<BasicForm 
+					label="Location"
+					type="text"
+					placeholder="Location" />
+				
+				<BasicForm 
+					label="Attendes"
+					type="number"
+					placeholder="Attendes" />
+
+				<BasicForm 
+					label="Information"
+					as="textarea"
+					rows={5}
+					placeholder="Title" />
+			</Form>
 		</>
 	);
 }
