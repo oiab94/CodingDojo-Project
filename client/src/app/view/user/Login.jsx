7fcd0b9b1ae7cc7250a1d0eb36da8bca4ab929c9
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 16:06:00 -0300

    [client] Permite logear a un usuario

--- client/src/app/view/user/Login.jsx
+++ client/src/app/view/user/Login.jsx
@@ -1,23 +1,58 @@
 import { Form, Button, Col } from "react-bootstrap";
 import BasicForm from "../../components/user/BasicForm";
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react";
 
 const LoginUser = () => {
+	const URL = "http://localhost:8000/api";
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+	const [ isEmailValid, setIsEmailValid ] = useState("");
+	const [ isPasswordValid, setIsPasswordValid ]= useState("");
+
+	const handleLogin = (event) => {
+		event.preventDefault();
+
+		axios.post(
+			URL + "/login",
+			{ email, password },
+			{withCredentials: true})
+			.then(user => {
+				window.localStorage.setItem("userID",user.data["userID"])
+			})
+			.catch(({ response }) => {
+				setIsEmailValid(response.data["email"]);
+				setIsPasswordValid(response.data["password"]);
+			})
+	}
+
 	return (
 		<>
-		<Form className="p-3">
+		<Form className="p-3" onSubmit={ handleLogin }>
 			<BasicForm
 				label="Email addres"
 				type="text"
-				placeholder="Enter email address" />
+				placeholder="Enter email address"
+				onChange={({ target }) => setEmail(target.value)} />
+			{
+				isEmailValid !== ""
+					? <p className="text-danger text-opacity-75">{ isEmailValid }</p>
+					: null
+			}
 
 			<BasicForm
 				label="Password"
 				type="password"
-				placeholder="Password" />
+				placeholder="Password" 
+				onChange={({ target }) => setPassword(target.value)} />
+			{
+				isPasswordValid !== ""
+					? <p className="text-danger text-opacity-75">{ isPasswordValid }</p>
+					: null
+			}
 
 			<Col>
-			<Button as={Link} to="/dashboard" variant="primary" type="submit">
+			<Button variant="primary" type="submit">
 				Login
 			</Button>
 			</Col>
