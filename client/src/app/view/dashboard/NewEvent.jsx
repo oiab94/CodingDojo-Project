63b2c1919501031f3d9a5ee31713e1cad39f550a
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-16 19:15:29 -0300

    [client] Permite crear un nuevo evento

--- client/src/app/view/dashboard/NewEvent.jsx
+++ client/src/app/view/dashboard/NewEvent.jsx
@@ -1,41 +1,119 @@
-import { Form } from "react-bootstrap";
+import axios from "axios";
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 import BasicForm from "../../components/user/BasicForm";
+import { useNavigate } from "react-router-dom";
 
 const NewEvent = () => {
+	const URL = "http://localhost:8000/api";
+	const navigate = useNavigate(); 
+	const [title, setTitle] = useState("");
+	const [date, setDate] = useState("");
+	const [time, setTime] = useState("");
+	const [location, setLocation] = useState("");
+	const [attendes, setAttendes] = useState("");
+	const [information, setInformation] = useState("");
+	// Conserva los errores
+	const [titleError, setTitleError] = useState("");
+	const [dateError, setDateError] = useState("");
+	const [locationError, setLocationError] = useState("");
+	const [timeError, setTimeError] = useState("");
+
+	// ! Handle Submit
+	const handleEventSubmit = ( event ) =>{
+		event.preventDefault();
+
+		axios.post(
+			URL + "/event",
+			{
+				title,
+				date,
+				time,
+				location,
+				attendes,
+				information 
+			},
+			{withCredentials: true})
+			.then(({ data }) => {
+				let { title, time, date, location} = data;
+				console.log({title, time, date, location});
+				navigate("/dashboard");
+			})
+			.catch(({ response }) => {
+				let { title, time, date, location} = response.data;
+				setTitleError(title);
+				setTimeError(time);
+				setDateError(date);
+				setLocationError(location);
+			})
+	}
+
 	return (
 		<>
 			<h1>New Event</h1>
-			<Form>
+			<Form onSubmit={ handleEventSubmit }>
 				<BasicForm 
 					label="Title"
 					type="text"
-					placeholder="Title" />
+					placeholder="Title"
+					onChange={({ target }) => setTitle(target.value)} />
+				{
+					titleError !== ""
+						? <p className="text-danger">{ titleError }</p>
+						: null
+				}
 
 				<BasicForm 
 					label="Date"
 					type="date"
-					placeholder="Date" />
-				
+					placeholder="Date" 
+					onChange={({ target }) => setDate(target.value)} />
+				{
+					dateError !== ""
+						? <p className="text-danger">{ dateError }</p>
+						: null
+				}
 				<BasicForm 
 					label="Time"
 					type="time"
-					placeholder="Time" />
+					placeholder="Time" 
+					onChange={({ target }) => setTime(target.value)} />
+				{
+					timeError !== ""
+						? <p className="text-danger">{ timeError }</p>
+						: null
+				}
+
 
 				<BasicForm 
 					label="Location"
 					type="text"
-					placeholder="Location" />
+					placeholder="Location" 
+					onChange={({ target }) => setLocation(target.value)} />
+				{
+					locationError !== ""
+						? <p className="text-danger">{ locationError }</p>
+						: null
+				}
+
 				
 				<BasicForm 
 					label="Attendes"
 					type="number"
-					placeholder="Attendes" />
+					placeholder="Attendes" 
+					onChange={({ target }) => setAttendes(target.value)} />
+
 
 				<BasicForm 
 					label="Information"
 					as="textarea"
 					rows={5}
-					placeholder="Title" />
+					placeholder="Title" 
+					onChange={({ target }) => setInformation(target.value)} />
+
+				<Button variant="primary" type="submit">
+					Create event
+				</Button>
 			</Form>
 		</>
 	);
